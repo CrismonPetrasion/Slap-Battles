0310f17a305bb88f0297bc25177bc667cd9b1751
CrismonPetrasion <148696199+CrismonPetrasion@users.noreply.github.com>
2023-10-28 14:36:07 +0300

    TroXer.lua

--- TroXer.lua
+++ TroXer.lua
@@ -32,6 +32,15 @@ if game.PlaceId == 6403373529 then
         })
     end
 
+    function Incorrect()
+        OrionLib:MakeNotification({
+            Name = "Error!",
+            Content = "The error has happended! Please try to report this bug to our discord!",
+            Image = "rbxassetid://4483345998",
+            Time = 5
+        })
+    end
+
     function IncorrectKeyNofitication()
         OrionLib:MakeNotification({
             Name = "Incorrect Key!",
@@ -42,7 +51,7 @@ if game.PlaceId == 6403373529 then
     end
 
     _G.InfiniteJump = false
-    _G.Key = "7z0q41yGlGs6U5gF"
+    _G.Key = "RELEASE"
     _G.KeyInput = "string"
     
 
@@ -63,12 +72,137 @@ if game.PlaceId == 6403373529 then
             PremiumOnly = false,
         })
 
+        local TeleportsTab = SystemWindow:MakeTab({
+            Name = "Teleports Settings",
+            Icon = "rbxassetid://4483345998",
+            PremiumOnly = false,
+        })
+
+        local BadgesTab = SystemWindow:MakeTab({
+            Name = "Badges Settings",
+            Icon = "rbxassetid://4483345998",
+            PremiumOnly = false,
+        })
+
         local MiscSection = MiscTab:AddSection({
             Name = "Miscellaneous"
         })
 
+        local BadgeSection = BadgesTab:AddSection({
+            Name = "Badges"
+        })
+
         local PlayerSection = PlayerTab:AddSection({
             Name = "Player"
+        })
+
+        local TeleportSection = TeleportsTab:AddSection({
+            Name = "Teleports"
+        })
+
+        local Button = BadgesTab:AddButton({
+            Name = "Get Lone Orange Badge, Get Court Evidence Badge",
+            Callback = function()
+                fireclickdetector(game.Workspace.Arena.island5.Orange.ClickDetector)
+                fireclickdetector(game.Workspace.Lobby.Scene.knofe.ClickDetector)
+            end
+        })
+
+        local Button2 = BadgesTab:AddButton({
+            Name = "Get Brazil Badge, Get Duck Badge",
+            Callback = function()
+                local Player = game.Players.LocalPlayer
+                local Character = Player.Character
+
+                Character.HumanoidRootPart.CFrame = game.workspace.Lobby.brazil.portal.CFrame
+
+                fireclickdetector(game.Workspace.Arena["default island"]["Rubber Ducky"].ClickDetector)
+            end
+        })
+
+        local Dropdown = TeleportsTab:AddDropdown({
+            Name = "Dropdown",
+            Defualt = "Input",
+            Options = {"Plate", "Safe Spot", "Defualt Arena", "Lobby", "Slapple Island", "Moai Island", "Arena", "Tournament"},
+            Callback = function(Value)
+                if Value == nil then
+                    Incorrect()
+                elseif Value == false then
+                    Incorrect()
+                elseif Value == true then
+                    Incorrect()
+                elseif Value == "Plate" then
+                    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace.Arena.Plate.CFrame * CFrame.new(0,2,0)
+                elseif Value == "Safe Spot" then
+                    local SafeSpot = Instance.new("Part", workspace)
+                    SafeSpot.Position = Vector3.new(-1500,100,-1500)
+                    SafeSpot.Name = "Spot"
+                    SafeSpot.Size = Vector3.new(50,50,50)
+                    SafeSpot.Anchored = true
+                    SafeSpot.Transparency = 0.7
+                    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace.Spot.CFrame * CFrame.new(0,28,0)
+                elseif Value == "Default  Arena" then
+                    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(120,360,-3)
+                elseif Value == "Arena" then
+                    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.workspace.Origo.CFrame * CFrame.new(0,-5,0)
+                elseif Value == "Lobby" then
+                    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-800,328,-2.5)
+                elseif Value == "Tournament" then
+                    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace.Battlearena.Arena.CFrame * CFrame.new(0,10,0)
+                elseif Value == "Slapple Island" then
+                    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.workspace.Arena.island5.Union.CFrame * CFrame.new(0,3.25,0)
+                elseif Value == "Moai Island" then
+                    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(215, -15.5, 0.5)
+                end
+            end
+        })
+
+        local DeathBlocks = MiscTab:AddToogle({
+            Name = "Anti Death Barriers, Cube",
+            Default = false,
+            Callback = function(Value)
+                if Value == true then
+                    for i,v in pairs(game.Workspace.DEATHBARRIER:GetChildren()) do
+                        if v.ClassName == "Part" and v.Name == "BLOCK" then
+                            v.CanTouch = false
+                        end
+                    end
+                    workspace.DEATHBARRIER.CanTouch = false
+                    workspace.DEATHBARRIER2.CanTouch = false
+                    workspace.dedBarrier.CanTouch = false
+                    workspace.ArenaBarrier.CanTouch = false
+                    workspace.AntiDefaultArena.CanTouch = false
+                    workspace.Arena.CubeOfDeathArea["the cube of death(i heard it kills)"].CanTouch = false
+
+                else
+                    for i,v in pairs(game.Workspace.DEATHBARRIER:GetChildren()) do
+                        if v.ClassName == "Part" and v.Name == "BLOCK" then
+                            v.CanTouch = true
+                        end
+                    end
+                    workspace.DEATHBARRIER.CanTouch = true
+                    workspace.DEATHBARRIER2.CanTouch = true
+                    workspace.dedBarrier.CanTouch = true
+                    workspace.ArenaBarrier.CanTouch = true
+                    workspace.AntiDefaultArena.CanTouch = true
+                    workspace.Arena.CubeOfDeathArea["the cube of death(i heard it kills)"].CanTouch = true
+
+                end
+            end
+        })
+        local AdminToogle = MiscTab:AddToogle({
+            Name = "Anti Admins",
+            Defualt = false,
+            Callback = function(Value)
+                AntiAdmins = Value
+                while AntiAdmins do
+                    for i,v in pairs(game.Players:GetChildren()) do
+                        if v:GetRankInGroup(9950771) >= 2 then
+                            game.Players.LocalPlayer:Kick("High Rank Player")
+                        end
+                    end
+                end
+            end
         })
 
         local Slider = PlayerTab:AddSlider({
@@ -127,6 +261,9 @@ if game.PlaceId == 6403373529 then
             Name = "Infinite Jump",
             Callback = function()
                 _G.InfiniteJump = true
+                if _G.InfiniteJump == false then
+                    Incorrect()
+                end
                 if _G.InfiniteJump == true then
                     local Bypass;
                     Bypass = hookmetamethod(game, "__namecall", function(method, ...) 
@@ -165,6 +302,7 @@ if game.PlaceId == 6403373529 then
                     DestroyPart:Destroy()
                 elseif Value == nil then
                     warn("Error")
+                    Incorrect()
                     Value = false
                 end
             end
